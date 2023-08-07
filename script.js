945978f84e7166a999ea7e4015b8f0e2d5c2577d
glaubersafreitas <glaubersafreitas@gmail.com>
2023-08-07 17:27:22 -0300

    Add functions in script.js

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ function toggleMode (){
  if(html.classList.contains("light")){
     //se tiver light mode, pegar a imagem light
     img.setAttribute("src", "./assets/meuavatar-light.png")
-    img.setAttribute("alt", "Foto de Glauber com óculos escuros ao lado da sua esposa")
+    img.setAttribute("alt", "Foto de Glauber Freitas, com óculos escuros ao lado da sua esposa")
 
  } else {
    //se tiver sem light mode, manter a imagem normal. 
    img.setAttribute("src", "./assets/meuavatar.png")
-   img.setAttribute("alt", "Foto de Glauber usando Jaqueta Cinza")
+   img.setAttribute("alt", "Foto de Glauber Freitas, usando Jaqueta Cinza")
  }
  
+}
+
+function ebook (){
+  alert ("Pedimos desculpas, mas no momento não temos nenhum e-book disponível para donwload!")
+}
+
+function nlw (){
+  alert ("No momento, não há inscrições abertas para o NLW! Em breve, teremos novidades para você!")
 }
 
 
